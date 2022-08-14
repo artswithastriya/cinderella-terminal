72dc48e5148d41cbcc1bf24dd98c5b8bbdd3a512
Nefo Fortressia <nefothingy@hotmail.com>
2022-08-14 20:16:34 +0700

    Add initial implementation of events() for Crossterm
    This is not done yet! Because the code to bridge Crossterm's events and our library's Event is not done yet!!
    
    Signed-off-by: Nefo Fortressia <nefothingy@hotmail.com>

--- src/backend/crossterm.rs
+++ src/backend/crossterm.rs
@@ -1,14 +1,46 @@
-use std::io::{stdout, Stdout};
-
+use async_std::task;
+use futures::{select, FutureExt};
+use futures_lite::stream::StreamExt;
+use futures_timer::Delay;
+use std::{
+    io::{stdout, Stdout},
+    sync::mpsc,
+    thread,
+};
 use tui::{backend::CrosstermBackend, Terminal};
 
-use crate::interface::Backend;
+use crate::{events::Event, interface::Backend};
 
 pub struct Crossterm;
 
 impl Backend for Crossterm {
-    fn events(tick_rate: std::time::Duration) -> std::sync::mpsc::Receiver<crate::events::Event> {
-        todo!()
+    fn events(tick_rate: std::time::Duration) -> mpsc::Receiver<crate::events::Event> {
+        let (tx, rx) = mpsc::channel();
+        let tx2 = tx.clone();
+        let mut stream = crossterm::event::EventStream::new();
+        task::spawn(async move {
+            loop {
+                let mut event = stream.next().fuse();
+                let mut delay = Delay::new(tick_rate).fuse();
+                select! {
+                    _ = delay => {   if let Err(err) = tx.send(Event::Tick) {
+                    eprintln!("{}", err);
+                    break;
+                } },
+                    maybe_event = event => {
+                        match maybe_event {
+                            Some(Ok(event)) => {
+                                tx.send(event.into());
+                            }
+                            Some(Err(e)) => println!("Error: {:?}\r", e),
+                            None => break,
+                        }
+                    }
+                };
+            }
+        });
+
+        rx
     }
 
     type TerminalArguments = CrosstermBackend<Stdout>;
@@ -17,5 +49,17 @@ impl Backend for Crossterm {
         let backend = CrosstermBackend::new(stdout());
 
         Ok(Terminal::new(backend)?)
+    }
+}
+
+impl From<crossterm::event::Event> for Event {
+    fn from(event: crossterm::event::Event) -> Self {
+        match event {
+            crossterm::event::Event::Key(key) => Self::Keyboard(match key {
+                _ => todo!()
+            }),
+            crossterm::event::Event::Mouse(_) => todo!(),
+            crossterm::event::Event::Resize(_, _) => todo!(),
+        }
     }
 }
