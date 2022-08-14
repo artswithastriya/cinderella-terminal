a7fbffd65fcc912ca6e36ab90a7ffe012f9dbf0c
Nefo Fortressia <nefothingy@hotmail.com>
2022-08-14 20:10:27 +0700

    Implement Auto backend
    Signed-off-by: Nefo Fortressia <nefothingy@hotmail.com>

--- src/backend/mod.rs
+++ src/backend/mod.rs
@@ -2,3 +2,15 @@
 pub mod crossterm;
 #[cfg(feature = "termion")]
 pub mod termion;
+
+pub mod auto {
+    cfg_if::cfg_if! {
+        if #[cfg(all(unix, feature = "termion"))] {
+            pub use super::termion::*;
+            pub use super::termion::Termion as Auto;
+        } else if #[cfg(any(feature = "crossterm", windows))] {
+            pub use super::crossterm::*;
+            pub use super::crossterm::Crossterm as Auto;
+        }
+    }
+}
